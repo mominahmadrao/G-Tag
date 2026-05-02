586c70e8d8abeb552fd5acbed729a928237a68f3
Uzair Sabir <jroy53371@gmail.com>
2026-05-02 19:59:24 +0300

    Added database connection

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./db/connectDB.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const port = process.env.PORT;
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`App listening on port http:localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB Connection error", err);
+    process.exit(1);
+  });
