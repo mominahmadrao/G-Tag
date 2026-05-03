4b197e1ff2529d9e07d56f2130871836c641ce86
mominahmadrao <186613410+mominahmadrao@users.noreply.github.com>
2026-05-03 18:30:29 +0500

    Add mailgen and nodemailer for mail functionality

--- src/controllers/authUser.controller.js
+++
@@ -1,6 +0,0 @@
-import User from "../models/users.models";
-import ApiError from "../utils/apiError.js";
-import asyncHandler from "../utils/asyncHandler.js"
-import jwt from "jsonwebtoken";
-
-export const
---
+++ src/middlewares/authUser.middleware.js
@@ -0,0 +1,49 @@
+import { User } from "../models/users.models";
+import ApiError from "../utils/apiError.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+
+
+const verifyJWT = asyncHandler(async function (req, res, next) {
+  const token =
+    req.cookies?.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    throw new ApiError(401, "Unauthorized access");
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    if (!decodedToken?._id) {
+      throw new ApiError(401, "Invalid token payload");
+    }
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+    );
+    if (!user) {
+      throw new ApiError(401, "Invalid access token");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, "Invalid access token");
+  }
+});
+
+const validateUserRole = (roles = []) => {
+  return asyncHandler(async (req, res, next) => {
+    if (!req.user) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+
+    if (!roles.includes(req.user.role)) {
+      throw new ApiError(403, "Forbidden: You don't have permission");
+    }
+
+    next();
+  });
+};
+
+export { verifyJWT, validateUserRole };
