cc3d075704ef3b1801b72cf86313c61f60a1a05d
mominahmadrao <186613410+mominahmadrao@users.noreply.github.com>
2026-05-03 17:11:09 +0500

    user schema done

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -74,9 +74,10 @@ const userSchema = new Schema(
 // 10 is cost factor
 // 10^1024 times repeated hashing internally before getting final result
 
-userSchema.pre("save", async function () {
-  if (!this.isModified("password")) return;
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
@@ -89,6 +90,7 @@ userSchema.methods.generateAccessToken = function () {
       _id: this._id,
       email: this.email,
       username: this.username,
+      role: this.role,
     },
     process.env.ACCESS_TOKEN_SECRET,
     {
@@ -103,6 +105,7 @@ userSchema.methods.generateRefreshToken = function () {
       _id: this._id,
       email: this.email,
       username: this.username,
+      role: this.role,
     },
     process.env.REFRESH_TOKEN_SECRET,
     { expiresIn: process.env.REFRESH_TOKEN_EXPIRY },
