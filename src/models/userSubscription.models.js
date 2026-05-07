96d2384a305ed091176bcc33a145821bb1b041a8
Uzair Sabir <jroy53371@gmail.com>
2026-05-07 06:34:23 +0300

    Added the setting for expiration date in user subscription.

--- src/models/userSubscription.models.js
+++ src/models/userSubscription.models.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import { SubscriptionStatusEnum } from "../utils/constants.js";
+import {ApiError} from "../utils/apiError.js";
 
 const userSubscriptionSchema = new Schema(
   {
@@ -24,6 +25,30 @@ const userSubscriptionSchema = new Schema(
   },
   { timestamps: true },
 );
+
+userSubscriptionSchema.pre("save", async function (next) {
+  if (!this.isModified("plan")) return next();
+
+  try {
+    const SubscriptionPlan = mongoose.model("SubscriptionPlan");
+    const planDetails = await SubscriptionPlan.findById(this.plan);
+
+    if (!planDetails) {
+      // Use your custom ApiError here for consistency
+      return next(new ApiError(404, "Subscription Plan not found"));
+    }
+
+    const expirationDate = new Date();
+    expirationDate.setDate(
+      expirationDate.getDate() + planDetails.durationInDays,
+    );
+    this.endDate = expirationDate;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 export const userSubscription = mongoose.model(
   "UserSubscription",
