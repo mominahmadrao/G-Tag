f4eb7e944cb592cb218761394e2b75ceb5d386be
Uzair Sabir <jroy53371@gmail.com>
2026-05-07 07:34:20 +0300

    Added more Routes for Order

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { placeOrder } from "../controllers/order.controllers.uzair.js";
+import {
+  placeOrder,
+  getMyOrders,
+  getOrderById,
+} from "../controllers/order.controllers.uzair.js";
 import { orderValidators } from "../validators/index.uzair.js";
 import { validate } from "../middleware/validate.middleware.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
@@ -7,6 +11,9 @@ import { verifyJWT } from "../middleware/auth.middleware.js";
 const router = Router();
 
 router.use(verifyJWT);
+
+router.route("/my-orders").get(getMyOrders);
+router.route("/:orderId").get(getOrderById);
 
 router.route("/").post(orderValidators.placeOrder, validate, placeOrder);
 
