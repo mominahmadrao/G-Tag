bb8cd552b2cf52b64a238f12444ff78b600fc3da
Uzair Sabir <jroy53371@gmail.com>
2026-05-07 07:04:03 +0300

    Added Cart Controllers

---
+++ src/controllers/cart.controllers.uzair.js
@@ -0,0 +1,79 @@
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { Cart } from "../models/carts.models.js";
+
+const addItemToCart = asyncHandler(async (req, res) => {
+  const { productId, quantity } = req.body;
+  const userId = req.user._id;
+
+  let cart = await Cart.findOne({ userId });
+
+  if (!cart) {
+    cart = await Cart.create({ userId, items: [], totalPrice: 0 });
+  }
+
+  cart.upsertItem(productId, quantity);
+
+  await cart.populate("items.productId");
+  cart.calculateTotalPrice();
+
+  await cart.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, cart, "Item added to cart successfully"));
+});
+
+const getCart = asyncHandler(async (req, res) => {
+  const cart = await Cart.findOne({ userId: req.user._id }).populate(
+    "items.productId",
+  );
+
+  if (!cart) {
+    throw new ApiError(404, "Cart not found");
+  }
+
+  cart.calculateTotalPrice();
+  await cart.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, cart, "Cart retrieved successfully"));
+});
+
+const removeItemFromCart = asyncHandler(async (req, res) => {
+  const { productId } = req.params;
+
+  const cart = await Cart.findOne({ userId: req.user._id });
+
+  if (!cart) throw new ApiError(404, "Cart not found");
+
+  cart.items = cart.items.filter(
+    (item) => item.productId.toString() !== productId,
+  );
+
+  await cart.populate("items.productId");
+  cart.calculateTotalPrice();
+  await cart.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, cart, "Item removed from cart"));
+});
+
+const clearCart = asyncHandler(async (req, res) => {
+  const cart = await Cart.findOne({ userId: req.user._id });
+
+  if (!cart) throw new ApiError(404, "Cart not found");
+
+  cart.items = [];
+  cart.totalPrice = 0;
+  await cart.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Cart cleared successfully"));
+});
+
+export { addItemToCart, getCart, removeItemFromCart, clearCart };
