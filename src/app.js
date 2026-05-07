3c3989fb85ba60fb094a43052fe034e45df676f3
Uzair Sabir <jroy53371@gmail.com>
2026-05-07 10:14:59 +0300

    Modified the app.js file to have all the routes and global error handling

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
+import { ApiError } from "./utils/apiError.js";
 
 const app = express();
+
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
@@ -10,5 +12,41 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+
+// Body Parsing Middleware
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// Route Imports
+import cartRouter from "./routes/cart.routes.js";
+import orderRouter from "./routes/order.routes.js";
+import paymentRouter from "./routes/payment.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import subscriptionPlanRouter from "./routes/subscriptionPlan.routes.js";
+
+// Route Mounts
+app.use("/api/v1/cart", cartRouter);
+app.use("/api/v1/orders", orderRouter);
+app.use("/api/v1/payments", paymentRouter);
+app.use("/api/v1/subscriptions/plans", subscriptionPlanRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use((err, req, res, next) => {
+  if (err instanceof ApiError) {
+    return res.status(err.statusCode).json({
+      success: err.success,
+      message: err.message,
+      errors: err.errors,
+      stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
+    });
+  }
+
+  return res.status(500).json({
+    success: false,
+    message: "Internal Server Error",
+  });
+});
 
 export default app;
